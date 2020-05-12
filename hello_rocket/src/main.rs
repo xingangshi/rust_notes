5080fcd1d347f15e0e4b4ec11b9102b155455e99
vincent <shix_112@163.com>
2020-05-12 22:06:18 +0800

    add some test codes

--- hello_rocket/src/main.rs
+++ hello_rocket/src/main.rs
@@ -11,18 +11,32 @@ mod other {
     }
 }
 
-#[get("/hello/<name>")]
-pub fn hello(name: &RawStr) -> String{
-    format!("Hello, {}!", name.as_str())
+// 动态路径
+#[get("/hello/<name>/<age>/<cool>")]
+pub fn hello(name: &RawStr, age: u8, cool: bool) -> String{
+    if cool {
+        format!("You are a cool {} year old, {} !", age, name)
+    }
+    else {
+        format!("{}, we need talk about your name. ", name.as_str())
+    }
 }
+
+#[get("/hello_1?wave&<name>")]
+fn hello_1(name: Option<String>) -> String {
+    name.map(| name | format!("Hi, {}!", name))
+        .unwrap_or_else(|| "Hello!".into())
+}
+
+
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, First rocket website"
 }
 
-use other::world;
+//use other::world;
 
 fn main() {
-    rocket::ignite().mount("/hello", routes![index, hello, other::world]).launch();
+    rocket::ignite().mount("/", routes![index, hello, hello_1, other::world]).launch();
 }
