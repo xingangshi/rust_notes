12b423e239be5b9cf3716485c83f71ab1da96ef3
vincent <shix_112@163.com>
2020-05-16 18:16:28 +0800

    Rust as a simple api server

--- hello_rocket/src/main.rs
+++ hello_rocket/src/main.rs
@@ -4,6 +4,11 @@
 
 use rocket::http::RawStr;
 
+use std::io::Cursor;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::{Request, Response};
+use rocket::http::{Header, ContentType, Method};
+
 mod other {
     #[get("/world")]
     pub fn world() -> &'static str{
@@ -22,12 +27,11 @@ pub fn hello(name: &RawStr, age: u8, cool: bool) -> String{
     }
 }
 
-#[get("/hello_1?wave&<name>")]
-fn hello_1(name: Option<String>) -> String {
-    name.map(| name | format!("Hi, {}!", name))
+#[get("/hello_1?<wave>&<name>")]
+fn hello_1(wave: String, name: Option<String>) -> String {
+    name.map(| name | format!("name :Hi, {}", name))
         .unwrap_or_else(|| "Hello!".into())
 }
-
 
 
 #[get("/")]
@@ -37,6 +41,37 @@ fn index() -> &'static str {
 
 //use other::world;
 
+
+struct CORS();
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response
+        }
+    }
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        if request.method() == Method::Options ||
+           response.content_type() == Some(ContentType::JSON) ||
+           response.content_type() == Some(ContentType::Plain) {
+
+            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+            response.set_header(Header::new("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS"));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type, Origin, X-Auth-Token"));
+            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        }
+
+        if request.method() == Method::Options {
+            response.set_header(ContentType::Plain);
+            response.set_sized_body(Cursor::new(""));
+        }
+    }
+}
+
+
 fn main() {
-    rocket::ignite().mount("/", routes![index, hello, hello_1, other::world]).launch();
+    rocket::ignite().attach(CORS()).mount("/", routes![index, hello, hello_1, other::world]).launch();
+
 }
